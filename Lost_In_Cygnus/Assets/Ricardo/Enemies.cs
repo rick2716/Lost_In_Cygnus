95243bc59a77c443b77092bc8476b5726d2d5a7c
ricardo-alvarez16 <89090386+rick2716@users.noreply.github.com>
2024-06-03 21:55:15 -0500

    Enemigos y Jefe

--- Lost_In_Cygnus/Assets/Ricardo/Enemies.cs
+++ Lost_In_Cygnus/Assets/Ricardo/Enemies.cs
@@ -6,27 +6,43 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class Enemies : MonoBehaviour
 {
+    [Header("RutinaEnemy")]
     public float wanderRadius; // Radio para rondar
     public float chaseRadius; // Radio para que el enemigo empiece a perseguir
     public float attackRadius; // Radio para empezar a atacar
     public float wanderTimer; // Tiempo antes de cambiar de posicion
+    private float timer;
+
+    [Header("Vida")]
+    public int maxVida;
+    public int vida;
+    public int damagePerHit;
 
     private Transform player; 
     private NavMeshAgent agent; 
-    private float timer; 
+    private Animator animator;
 
-    private Animator animator; 
+    [Header("CollidersAtaquesSoloMuscomorphSinAlas")]
+    public GameObject colliderBite;
+    public GameObject colliderSting;
+
+    [Header("CollidersAtaqueArthromahre")]
+    public GameObject colliderClaw1;
+    public GameObject colliderClaw2;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform; 
         agent = GetComponent<NavMeshAgent>();
         timer = wanderTimer;
-        animator = GetComponent<Animator>(); 
+        animator = GetComponent<Animator>();
+        vida = maxVida;
     }
 
     void Update()
     {
+        animator.SetBool("Move", false);
+        animator.SetBool("Attack", false);
         timer += Time.deltaTime;
 
         float distanceToPlayer = Vector3.Distance(player.position, transform.position);
@@ -41,6 +57,9 @@ public class Enemies : MonoBehaviour
         }
         else
         {
+            animator.SetBool("Move", true);
+            animator.SetBool("Attack", false);
+
             // Rondar aleatoreamente
             if (timer >= wanderTimer)
             {
@@ -53,13 +72,16 @@ public class Enemies : MonoBehaviour
 
     void Chase()
     {
+        animator.SetBool("Move", true);
+        animator.SetBool("Attack", false);
         agent.SetDestination(player.position);
     }
 
     void Attack()
     {
         agent.SetDestination(transform.position);
-        //animator.SetTrigger("Attack");
+        animator.SetBool("Attack", true);
+        animator.SetBool("Move", false);
     }
 
     public Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
@@ -73,5 +95,53 @@ public class Enemies : MonoBehaviour
         NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
 
         return navHit.position;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        vida -= damage;
+        if(damage <= 0)
+        {
+            agent.SetDestination(transform.position);
+            animator.SetTrigger("Die");
+        }
+    }
+
+    // METODOS PARA PRENDER COLLIDERS MUSCOMORPH (BICHO PEQUEÑO)
+    public void OnStingAttack()
+    {
+        colliderSting.SetActive(true);
+    }
+
+    public void OffStingAttack()
+    {
+        colliderSting.SetActive(false);
+        animator.SetBool("Move", true);
+        animator.SetBool("Attack", false);
+    }
+    public void OnBiteAttack()
+    {
+        colliderBite.SetActive(true);
+    }
+
+    public void OffBiteAttack()
+    {
+        colliderBite.SetActive(false);
+    }
+
+    // METODOS PARA PRENDER COLLIDERS Arthromahre (bicho grande)
+    public void OnClawAttack()
+    {
+        colliderClaw1.SetActive(true);
+        colliderClaw2.SetActive(true);
+    }
+
+    public void OffClawAttack()
+    {
+        colliderClaw1.SetActive(false);
+        colliderClaw2.SetActive(false);
+
+        animator.SetBool("Move", true);
+        animator.SetBool("Attack", false);
     }
 }
